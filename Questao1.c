4ba89621256414b2616d58c45775f3a322c4253f
Rodrigo Fonseca <164562915+Tylapias@users.noreply.github.com>
2025-05-29 10:54:31 -0300

    Create Questao1.c

---
+++ Questao1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x,y;
+    printf("Insira o primeiro número: \n");
+    scanf("%d", &x);
+    printf("Insira o segundo número: \n");
+    scanf("%d", &y);
+    
+    printf("Soma: %d", x+y);
+    return 0;
+    
+}
